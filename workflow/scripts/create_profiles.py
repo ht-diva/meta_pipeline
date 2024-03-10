d3d0eec1ebf4a8406074ad76b4f61370fbe630e5
Gianmauro Cuccuru <gianmauro.cuccuru@fht.org>
2024-03-10 23:11:41 +0100

    first import

---
+++ workflow/scripts/create_profiles.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+
+def main(overlap_map, template_file, outdir):
+    with open(template_file, "r") as fp:
+        lines = fp.readlines()
+
+    seqid = Path(outdir).name
+
+    with open(Path(outdir, f"profile_{seqid}.txt"), "w") as fp:
+        for line in lines:
+            line = line.replace("input_study_one", overlap_map.loc[seqid, 'file_path_study_one'], 1)
+            line = line.replace("input_study_two", overlap_map.loc[seqid, 'file_path_study_two'], 1)
+            line = line.replace("output_path", outdir, 1)
+            fp.write(line)
+
+
+main(overlap_map=snakemake.params.overlap_map,
+     template_file=snakemake.input.template,
+     outdir=snakemake.params.outdir)

4fe1f51a21824ef7cdb05d334fa33938a9821373
Gianmauro Cuccuru <gianmauro.cuccuru@fht.org>
2024-03-11 13:16:58 +0100

    fix var name

--- workflow/scripts/create_profiles.py
+++ workflow/scripts/create_profiles.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 
-def main(overlap_map, template_file, outdir):
+def main(matching_map, template_file, outdir):
     with open(template_file, "r") as fp:
         lines = fp.readlines()
 
@@ -9,12 +9,12 @@ def main(overlap_map, template_file, outdir):
 
     with open(Path(outdir, f"profile_{seqid}.txt"), "w") as fp:
         for line in lines:
-            line = line.replace("input_study_one", overlap_map.loc[seqid, 'file_path_study_one'], 1)
-            line = line.replace("input_study_two", overlap_map.loc[seqid, 'file_path_study_two'], 1)
+            line = line.replace("input_study_one", matching_map.loc[seqid, 'file_path_study_one'], 1)
+            line = line.replace("input_study_two", matching_map.loc[seqid, 'file_path_study_two'], 1)
             line = line.replace("output_path", f"{outdir}/METAL_output_{seqid}_", 1)
             fp.write(line)
 
 
-main(overlap_map=snakemake.params.matching_map,
+main(matching_map=snakemake.params.matching_map,
      template_file=snakemake.input.template,
      outdir=snakemake.params.outdir)

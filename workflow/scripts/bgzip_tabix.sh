b53cde5be4a784b54f121a51c9c6a61cef1863b5
Gianmauro Cuccuru <gianmauro.cuccuru@fht.org>
2024-06-12 07:48:50 +0200

    update the bgzip_tabix script to be more resilient and add header to the index

--- workflow/scripts/bgzip_tabix.sh
+++ workflow/scripts/bgzip_tabix.sh
@@ -4,6 +4,21 @@ INPUT=$1
 THREADS=$2
 DIRNAME=`dirname ${1}`
 FILENAME=`basename ${1} .gz`
+EXT=".bgz"
+OUTPUT="${DIRNAME}/${FILENAME}${EXT}"
+TMPFILE=$(mktemp $TMPDIR/foo-XXXXXX)
+
 
 # tabix indexes CHROM and POS columns
-gunzip ${INPUT} && bgzip --threads ${THREADS} ${DIRNAME}/${FILENAME} && tabix -f -S 1 -s 1 -b 2 -e 2 ${INPUT}
+echo ${OUTPUT} && \
+gunzip -c ${INPUT} >  ${TMPFILE} && \
+sed -i -e 's/CHR/#CHR/g' ${TMPFILE} && \
+bgzip --stdout --threads ${THREADS}  ${TMPFILE} > ${OUTPUT} && \
+tabix -f -s 1 -b 2 -e 2 ${OUTPUT} && \
+num_lines=$(tabix -l "$OUTPUT" | wc -l) && \
+if [[ "$num_lines" -eq 22 ]]; then
+	echo "Number of lines in the index: $num_lines"
+else
+	echo "Wrong number of lines in the index: $num_lines"
+	exit 1
+fi
